5a8d45519b171db1c95b023b69bf98939c1e7bfd
prakashdahal2005 <prakashdahal2005@gmail.com>
2025-11-14 00:03:35 +0545

    improved a little bit of ui :)

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -59,7 +59,9 @@
         </div>
     </div>
 </div>
+@auth
 @if(auth()->user()->profile->id === $profile->id)
 <p><a style="color:blue;" href="{{ route('profiles.edit') }}">Edit profile</a></p>
 @endif
+@endauth
 @endsection
